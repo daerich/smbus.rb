9be08a94715225faa96f48047c647c7e1eb6ee45
Erich Ericson <fakefakefans@gmail.com>
2022-01-07 17:09:28 +0100

    Objectify, fix logical error in condition

--- smbus.c
+++ smbus.c
@@ -22,32 +22,36 @@
 #include <linux/i2c.h>
 #include <i2c/smbus.h>
 
-static VALUE read_byte_data(VALUE self, VALUE fd, VALUE cmd)
+static VALUE read_byte_data(VALUE self, VALUE cmd)
 {
 	int ret = 0;
+	int fd = 0;
 
-	Check_Type(fd, T_FIXNUM);
 	Check_Type(cmd, T_FIXNUM);
-	ret = i2c_smbus_read_byte_data(FIX2INT(fd), FIX2INT(cmd));
+	fd = NUM2INT(rb_funcall(self, rb_intern("fileno"), 0));
+	ret = i2c_smbus_read_byte_data(fd, FIX2INT(cmd));
 	if (ret < 0) {
-		rb_raise(rb_eIOError, "SMBUS Read operation failed!");
+		rb_raise(rb_eIOError, "SMBUS Read operation failed!\
+Got Code: %d", ret);
 		return Qnil;
 	} else {
 		return INT2FIX(ret);
 	}
 }
 
-static VALUE write_byte_data(VALUE self, VALUE fd, VALUE reg, VALUE bit)
+static VALUE write_byte_data(VALUE self, VALUE reg, VALUE bit)
 {	
 	int ret = 0;
+	int fd = 0;
 
-	Check_Type(fd, T_FIXNUM);
 	Check_Type(reg, T_FIXNUM);
 	Check_Type(bit, T_FIXNUM);
-	ret = i2c_smbus_write_byte_data(FIX2INT(fd), FIX2INT(reg), FIX2INT(bit));
-	if (!ret) {
-		rb_raise(rb_eIOError, "SMBUS Write operation failed!");
-		return Qnil;
+	fd = NUM2INT(rb_funcall(self, rb_intern("fileno"), 0));
+	ret = i2c_smbus_write_byte_data(fd, FIX2INT(reg), FIX2INT(bit));
+	if (ret) {
+		rb_raise(rb_eIOError, "SMBUS Write operation failed!\
+Got Code: %d", ret);
+		return INT2FIX(ret);
 	} else {
 		return INT2FIX(ret);
 	}
@@ -56,6 +60,6 @@ static VALUE write_byte_data(VALUE self, VALUE fd, VALUE reg, VALUE bit)
 void Init_Smbus(void)
 {
 	VALUE mod_smbus = rb_define_module("Smbus");
-	rb_define_module_function(mod_smbus, "read_data",read_byte_data, 2);
-	rb_define_module_function(mod_smbus, "write_data", write_byte_data, 3);
+	rb_define_method(mod_smbus, "read_data",read_byte_data, 1);
+	rb_define_method(mod_smbus, "write_data", write_byte_data, 2);
 }

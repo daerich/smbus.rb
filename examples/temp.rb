9517f82d5c69a1607afe69e413ac2bb6e66d7839
Erich Ericson <fakefakefans@gmail.com>
2022-01-09 15:47:36 +0100

    examples/temp.rb: Improved error checking/handling, formatting

--- examples/temp.rb
+++ examples/temp.rb
@@ -13,6 +13,7 @@
 # LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
 # OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
+
 require "Smbus"
 
 module Const
@@ -31,7 +32,7 @@ end
 
 begin
   i2c = File.open("/dev/i2c-1","r+")
-  raaise("ioctl failed") unless i2c.ioctl(Const::I2C_SLAVE, Const::DEV_ID) >= 0
+  raise(IOError,"ioctl failed") unless i2c.ioctl(Const::I2C_SLAVE, Const::DEV_ID) >= 0
   i2c.read_data(Const::WHO_AM_I)
   i2c.write_data(Const::REG1, 0x00)
   i2c.write_data(Const::REG1, 0x84)
@@ -40,14 +41,15 @@ begin
   temp_l = i2c.read_data(Const::TEMP_L)
   temp_h = i2c.read_data(Const::TEMP_H)
   temp = (temp_h << 8 | temp_l)
-  # Overflow calculation /Simulation
+  # Overflow calculation/Simulation
   temp = (((temp + 2 ** 15) % 2 ** 16) - 2**15) if temp > 32767
   puts((42.5 + (temp / 480.0)).truncate(2))
   i2c.write_data(Const::REG1, 0x00)
-rescue
-  $stderr.puts "Reading of temperature failed at #{`date`}"
+rescue => error
+  $stderr.puts "Reading of temperature failed at #{`date`} \
+  Reason: #{error.message}"
   exit(1)
 ensure
-  i2c.close
+  i2c.close if defined? i2c.close
 end
 exit(0)

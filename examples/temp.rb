b14758a55c11704fe1b17dce2462171cb46e2d72
Erich Ericson <fakefakefans@gmail.com>
2022-01-10 16:54:50 +0100

    examples/temp.rb: Objectify, add class SenseHat

--- examples/temp.rb
+++ examples/temp.rb
@@ -24,32 +24,55 @@ module Const
   REG2 = 0x21
   TEMP_L = 0x2B
   TEMP_H = 0x2C
+  PRES_XL = 0x28
+  PRES_L = 0x29
+  PRES_H = 0x2A
 end
 
 class File
   include Smbus
 end
 
-begin
-  i2c = File.open("/dev/i2c-1","r+")
-  raise(IOError,"ioctl failed") unless i2c.ioctl(Const::I2C_SLAVE, Const::DEV_ID) >= 0
-  i2c.read_data(Const::WHO_AM_I)
-  i2c.write_data(Const::REG1, 0x00)
-  i2c.write_data(Const::REG1, 0x84)
-  i2c.write_data(Const::REG2, 0x01)
-  sleep 0.4 until i2c.read_data(Const::REG2) == 0;
-  temp_l = i2c.read_data(Const::TEMP_L)
-  temp_h = i2c.read_data(Const::TEMP_H)
-  temp = (temp_h << 8 | temp_l)
-  # Overflow calculation/Simulation
-  temp = (((temp + 2 ** 15) % 2 ** 16) - 2**15) if temp > 32767
-  puts((42.5 + (temp / 480.0)).truncate(2))
-  i2c.write_data(Const::REG1, 0x00)
-rescue => error
-  $stderr.puts "Reading of temperature failed at #{`date`} \
-  Reason: #{error.message}"
-  exit(1)
-ensure
-  i2c.close if defined? i2c.close
+class SenseHat
+
+  def initialize
+    @i2c = File.open("/dev/i2c-1","r+")
+    raise(IOError,"ioctl failed") unless @i2c.ioctl(Const::I2C_SLAVE, Const::DEV_ID) >= 0
+    raise(IOError,"who-am-i error") unless @i2c.read_data(Const::WHO_AM_I) == 189
+  end
+  def close
+      @i2c.close if defined? @i2c.close
+  end
+  def setup
+    @i2c.write_data(Const::REG1, 0x00)
+    @i2c.write_data(Const::REG1, 0x84)
+    @i2c.write_data(Const::REG2, 0x01)
+    sleep 0.4 until @i2c.read_data(Const::REG2) == 0
+  end
+  def teardown
+    @i2c.write_data(Const::REG1, 0x00)
+  end
+  alias_method :clear, :teardown
+  def getTemperature
+    setup
+    temp_l = @i2c.read_data(Const::TEMP_L)
+    temp_h = @i2c.read_data(Const::TEMP_H)
+    temp = (temp_h << 8 | temp_l)
+    # Overflow calculation/Simulation
+    temp = (((temp + 2 ** 15) % 2 ** 16) - 2**15) if temp > 32767
+    teardown
+    (42.5 + (temp / 480.0)).truncate(2)
+  end
+  def getPressure
+    press_xl = @i2c.read_data(Const::PRES_XL)
+    press_l = @i2c.read_data(Const::PRES_L)
+    press_h = @i2c.read_data(Const::PRES_H)
+    press = press_h << 16 | press_l << 8 | press_xl
+    teardown
+    press / 4096
+  end
+  private :setup, :teardown
 end
-exit(0)
+sense = SenseHat.new
+puts sense.getTemperature
+sense.close
